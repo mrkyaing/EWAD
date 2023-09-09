12821da5e18e2ee8d6654680fff1c36186be9116
Mg Kyaing <mgkyaing@globalwave.company>
2023-09-09 20:59:47 +0630

    JQuery & AJAX Practice with ASP.NET CORE MVC

---
+++ JQueryAjaxPractice/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using JQueryAjaxPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JQueryAjaxPractice.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult GetTimeNow() => View();//it will render only view UI
+        public JsonResult GetCurrentTime()
+        {
+            string now = DateTime.Now.ToString();
+            return Json(now);
+        }
+
+        public IActionResult MakeAnOrder()=>View();
+        [HttpPost]
+        public JsonResult MakeAnOrder(OrderModel order)
+        {
+            int totalAmount = order.Quantity * order.UnitPrice;//2*1500
+            return Json(totalAmount);//3000
+        }
+    }
+}
---
+++ JQueryAjaxPractice/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JQueryAjaxPractice.Models
+{
+    public class OrderModel
+    {
+        public string Id { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
