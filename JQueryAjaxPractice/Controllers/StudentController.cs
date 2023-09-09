1a205909f8f5ab96f5804bd8c1fcb10e9bc0d14e
Mg Kyaing <mgkyaing@globalwave.company>
2023-09-09 22:07:09 +0630

    practice with model binding in .net core mvc

---
+++ JQueryAjaxPractice/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using JQueryAjaxPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JQueryAjaxPractice.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Entry() => View();
+
+        [HttpPost]
+        public IActionResult Entry(StudentModel student)
+        {
+            if(student is not null)
+            {
+                ViewBag.Info = $"Hello,{student.LastName} {student.LastName},you are successfully registerd";
+            }
+           
+            return View();
+        }
+    }
+}
---
+++ JQueryAjaxPractice/Models/StudentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JQueryAjaxPractice.Models
+{
+    public class StudentModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Address { get; set; }
+    }
+}
