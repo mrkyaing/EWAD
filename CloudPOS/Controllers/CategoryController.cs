d5d329d419e8a3df0fd36d06c8a1c4cd66a13a93
Mg Kyaing <mgkyaing@globalwave.company>
2023-09-23 20:39:50 +0630

    integrate with Datatabble .net ui for sorting and searching .

--- CloudPOS/Controllers/CategoryController.cs
+++ CloudPOS/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using CloudPOS.Models;
 using CloudPOS.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CloudPOS.Controllers
 {
@@ -67,6 +68,49 @@ namespace CloudPOS.Controllers
             catch (Exception e)
             {
                 TempData["Info"] = "Error occur when delete a record to the system !" + e.Message;
+            }
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Edit(string Id)//1
+        {
+                CategoryViewModel categoryViewModel = _context.Categories.AsNoTracking().Where(x => x.Id == Id)
+                .Select(s=>new CategoryViewModel
+                {
+                 Id=s.Id,
+                 Code=s.Code,
+                 Description=s.Description
+                }).FirstOrDefault();//Method style Linq 
+            //CategoryEntity category1 = (from c in _context.Categories where c.Id == Id select c).FirstOrDefault();//Query Style Linq
+            if (categoryViewModel != null)
+                return View(categoryViewModel);
+            else
+            {
+                TempData["Info"] = "There is no record that you selected";
+                return RedirectToAction("List");
+            }
+        }
+        [HttpPost]
+       public IActionResult Update(CategoryViewModel viewModel)
+        {
+            try
+            {
+                //Data Transfer from viewModel to Entity
+                var categoryEntity = new CategoryEntity()
+                {
+                    Id = viewModel.Id,
+                    Code = viewModel.Code,
+                    Description = viewModel.Description,
+                    ModifiedAt = DateTime.Now
+                };
+                _context.Entry(categoryEntity).State = EntityState.Modified;
+                _context.SaveChanges();
+                TempData["Info"] = "Successfully update a record to the system";
+            }
+            catch (Exception e)
+            {
+
+                TempData["Info"] = "Error occur when update a record to the system !" + e.Message;
             }
             return RedirectToAction("List");
         }
