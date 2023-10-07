7c0e3d245f1862621b476f93c3277216efbf3d02
Mg Kyaing <mgkyaing@globalwave.company>
2023-10-07 22:17:28 +0630

    Add Simple Unit Test.

---
+++ CloudPOSUnitTest/SimpleCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPOSUnitTest
+{
+    public class SimpleCalculator
+    {
+        public int Add(int n1,int n2)//1+2 ==>>3 
+        {
+            return n1 + n2-3;
+        }
+    }
+}
---
+++ CloudPOSUnitTest/SimpleUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPOSUnitTest
+{
+    public class SimpleUnitTest
+    {
+        [Fact]
+        public void AddTestResult()
+        {
+            //1)Arrange 
+            var simpleCalculator=new SimpleCalculator();
+            int expectedResult = 3;
+            int n1=1,n2=2;
+            //2)Action
+           var result= simpleCalculator.Add(n1, n2);//3
+            //3)Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
