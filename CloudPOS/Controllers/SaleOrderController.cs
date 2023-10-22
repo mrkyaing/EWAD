334986dc387482c6882a944b5f6e2acba5557fc3
Mg Kyaing <mgkyaing@globalwave.company>
2023-10-22 22:37:00 +0630

    addToCart Functions

--- CloudPOS/Controllers/SaleOrderController.cs
+++ CloudPOS/Controllers/SaleOrderController.cs
@@ -1,4 +1,5 @@
-﻿using CloudPOS.Services;
+﻿using CloudPOS.Models.ViewModels;
+using CloudPOS.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudPOS.Controllers
@@ -6,14 +7,27 @@ namespace CloudPOS.Controllers
     public class SaleOrderController : Controller
     {
         private readonly ISaleProcessService _saleProcessService;
-
-        public SaleOrderController(ISaleProcessService saleProcessService)
+        private readonly IItemService _itemService;
+        public IList<SaleDetailViewModel> saleDetails=new List<SaleDetailViewModel>();
+        public SaleOrderController(ISaleProcessService saleProcessService,IItemService itemService)
         {
             _saleProcessService = saleProcessService;
+            _itemService = itemService;
         }
         public IActionResult Entry()
         {
+            ViewBag.Items = _itemService.GetAll();
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddToCart(SaleViewModel sale,SaleDetailViewModel saleDetail)
+        {
+            saleDetails.Add(saleDetail);
+            ViewBag.Info = "adding an item to the cart!!";
+            ViewBag.Items = _itemService.GetAll();
+            ViewBag.VoucherNo = sale.VoucherNo;
+            ViewBag.SaledDate = sale.SaledDate;
+            return View("Entry");
         }
         public IActionResult List()
         {
